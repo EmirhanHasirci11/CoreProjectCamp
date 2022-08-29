5ab13169e3c7289288e69cecaabb172caeecd2d4
Emirhan Hasırcı <21010310068@ogrenci.bartin.edu.tr>
2022-08-29 03:40:27 +0300

    Initial Commit
    Sertifika 1/7 Görevi

---
+++ DataAccessLayer/Concrete/Repositories/GenericRepository.cs
@@ -0,0 +1,45 @@
+﻿using DataAccessLayer.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete.Repositories
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        public T GetById(int id)
+        {
+            using var c = new Context();
+            return c.Set<T>().Find(id);
+        }
+
+        public List<T> GetList()
+        {
+            using var c = new Context();
+            return c.Set<T>().ToList();
+        }
+
+        public void TAdd(T t)
+        {
+            using var c = new Context();
+            c.Add(t);
+            c.SaveChanges();
+        }
+
+        public void TDelete(T t)
+        {
+            using var c = new Context();
+            c.Remove(t);
+            c.SaveChanges();
+        }
+
+        public void TUpdate(T t)
+        {
+            using var c = new Context();
+            c.Update(t);
+            c.SaveChanges();
+        }
+    }
+}
